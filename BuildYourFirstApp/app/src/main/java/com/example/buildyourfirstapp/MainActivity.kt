74ad43fa2e0a4593ee1664645e0942c81e1d92d6
N0341 <n0341@student.jamk.fi>
2019-05-30 12:14:19 +0300

    BuildYourFirstApp is ready

---
+++ BuildYourFirstApp/app/src/main/java/com/example/buildyourfirstapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.buildyourfirstapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val EXTRA_MESSAGE = "com.example.buildyourfirstapp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun showMessage(view: View){
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+
+    }
+}
