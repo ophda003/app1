ea14c15a0485c5172ea8ccc63ecaba59ea77296a
N0341 <n0341@student.jamk.fi>
2019-06-07 20:48:21 +0300

    RoomShoppingList is ready

---
+++ RoomShoppingList/app/src/main/java/com/example/roomshoppinglist/ShoppingListDao.kt
@@ -0,0 +1,19 @@
+package com.example.roomshoppinglist
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+
+@Dao
+interface ShoppingListDao {
+
+    @Query("SELECT * from shopping_list_table")
+    fun getAll(): MutableList<ShoppingListItem>
+
+    @Insert
+    fun insert(item: ShoppingListItem) : Long
+
+    @Query("DELETE FROM shopping_list_table WHERE id = :itemId")
+    fun delete(itemId: Int)
+
+}
